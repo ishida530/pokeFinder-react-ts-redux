318205008181c8a5bc24a30d1dc464b179fa6f50
pawelsawczuk <pawel.sawczuk@ekookna.pl>
2021-09-23 15:39:40 +0200

    first commit

---
+++ src/actions/PokemonAction.ts
@@ -0,0 +1,23 @@
+import { Dispatch } from "redux"
+import { PokemonDispatchTypes, POKEMON_SUCCESS, POKEMON_FAIL, POKEMON_LOADING } from "./PokemonActionTypes"
+import axios from "axios";
+
+export const GetPokemon = (pokemon: string) => async (dispatch: Dispatch<PokemonDispatchTypes>) => {
+    try {
+        dispatch( {
+            type: POKEMON_LOADING
+        })
+
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+        dispatch({
+            type: POKEMON_SUCCESS,
+            payload: res.data
+        })
+    } catch (e) {
+        dispatch( {
+            type: POKEMON_FAIL
+        })
+
+    }
+};
